ea726716dc6a9e52ef1b1e016b4af6dc97d6bdb5
Samuel Näsström <nasstromsamuel@icloud.com>
2022-12-30 09:54:44 +0000

    added likes detail view and urls

--- likes/serializers.py
+++ likes/serializers.py
@@ -1,20 +1,18 @@
 from django.db import IntegrityError
 from rest_framework import serializers
-from .models import Like
+from likes.models import Like
 
 
 class LikeSerializer(serializers.ModelSerializer):
     """
-    Serializer for the Comment model
-    Adds three extra fields when returning a list of Comment instances
+    Serializer for the Like model
+    The create method handles the unique constraint on 'owner' and 'post'
     """
     owner = serializers.ReadOnlyField(source='owner.username')
 
     class Meta:
-        model = Like()
-        fields = [
-            'id', 'owner', 'post', 'created_at',
-        ]
+        model = Like
+        fields = ['id', 'created_at', 'owner', 'post']
 
     def create(self, validated_data):
         try:
