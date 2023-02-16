e9a65adf5e8c8096c7cd6c97c9b1a1c692535033
Samuel Näsström <nasstromsamuel@icloud.com>
2023-02-16 14:21:41 +0000

    added private_messages app and model

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -45,10 +45,11 @@ REST_AUTH_SERIALIZERS = {
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
-ALLOWED_HOSTS = ['localhost', 'drf-api-sam.herokuapp.com']
+ALLOWED_HOSTS = [
+    'localhost', 'drf-api-sam.herokuapp.com', 'lifeshare.herokuapp.com']
 # Application definition
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -74,6 +75,7 @@ INSTALLED_APPS = [
     'comments',
     'likes',
     'followers',
+    'private_messages'
 ]
 SITE_ID = 1
 MIDDLEWARE = [
