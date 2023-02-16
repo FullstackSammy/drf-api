73fd4231c52a64369d4ad8389efb46cbca6388a2
Samuel Näsström <nasstromsamuel@icloud.com>
2023-02-16 19:27:06 +0000

    connected the urls for privatemessages and privatemessagedetail

--- private_messages/urls.py
+++ private_messages/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import PrivateMessageList, PrivateMessageDetail
 
 urlpatterns = [
-    path('messages/', PrivateMessageList.as_view(), name='message-list'),
-    path('messages/<int:pk>', PrivateMessageDetail().as_view(), name='message-detail'),
+    path('', PrivateMessageList.as_view(), name='private_message_list'),
+    path('<int:pk>/', PrivateMessageDetail.as_view(), name='private_message_detail'),
 ]
--- private_messages/views.py
+++ private_messages/views.py
@@ -1,7 +1,7 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 from .models import PrivateMessage
-from .serializer import PrivateMessageSerializer
+from .serializers import PrivateMessageSerializer
 
 
 class PrivateMessageList(generics.ListCreateAPIView):
